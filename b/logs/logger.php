3de6cca154a11b377f8774b1c3eb358745ef22be
Nic0uu <nic0uu@users.noreply.github.com>
2026-08-19 20:45:31 -0300

    Eliminar carpeta b

--- b/logs/logger.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function registrar_log($mensaje, $tipo = 'INFO') {
-    $archivo_log = __DIR__ . '/accesos.log';
-    $fecha = date('Y-m-d H:i:s');
-    $linea = "[$fecha] [$tipo] $mensaje" . PHP_EOL;
-
-    file_put_contents($archivo_log, $linea, FILE_APPEND | LOCK_EX);
-}
